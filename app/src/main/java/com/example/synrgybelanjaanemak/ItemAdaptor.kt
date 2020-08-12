0ae98fd18e011196a85c7589bc50ae1aade309b1
AHMAD ABRAR <ahmad.abrar@mail.ugm.ac.id>
2020-08-12 20:58:02 +0700

    Adaptor

---
+++ app/src/main/java/com/example/synrgybelanjaanemak/ItemAdaptor.kt
@@ -0,0 +1,50 @@
+package com.example.synrgybelanjaanemak
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recvi_item_belanjaan.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class ItemAdaptor (val listItem: List<Item>) : RecyclerView.Adapter<ItemAdaptor.ViewHolder>() {
+    private lateinit var db: ItemDatabase
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recvi_item_belanjaan,parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listItem.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.et_cv_nama.setText(listItem[position].name)
+        holder.itemView.et_cv_quantity.setText(listItem[position].quantity.toString())
+        holder.itemView.et_cv_satuan.setText(listItem[position].satuan.toString())
+        holder.itemView.et_cv_hargasatuan.setText(listItem[position].hargaSatuan.toString())
+
+        ItemDatabase.getInstance(holder.itemView.context)?.let {
+            db =it
+        }
+        holder.itemView.iv_delete.setOnClickListener{
+            GlobalScope.launch {
+                val totalRowDeleted = db.itemDao().deleteItem(listItem[position])
+                (holder.itemView.context as MainActivity).runOnUiThread{
+                    if (totalRowDeleted > 0) {
+                        Toast.makeText(holder.itemView.context, "Data ${listItem[position].name} Sukses Dihapus", Toast.LENGTH_LONG).show()
+                    } else{
+                        Toast.makeText(holder.itemView.context, "Data ${listItem[position].name} Gagal Dihapus", Toast.LENGTH_LONG).show()
+                    }
+                }
+                (holder.itemView.context as MainActivity).fetchData()
+            }
+        }
+
+        
+    }
+}
--- app/src/main/java/com/example/synrgybelanjaanemak/MainActivity.kt
+++ app/src/main/java/com/example/synrgybelanjaanemak/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.synrgybelanjaanemak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var db : ItemDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ItemDatabase.getInstance(this)?.let {
+            db = it
+        }
+
+        fab.setOnClickListener{
+            val intentGoToActivityAdd = Intent(this, AddActivity::class.java)
+            startActivity(intentGoToActivityAdd)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fetchData()
+    }
+    fun fetchData(){
+        GlobalScope.launch {
+            val listItem = db.itemDao().readAllItem()
+
+            runOnUiThread {
+                val adapter = ItemAdapter(listItem)
+                rv_layout.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+                rv_layout.adapter = adapter
+            }
+        }
     }
 }
