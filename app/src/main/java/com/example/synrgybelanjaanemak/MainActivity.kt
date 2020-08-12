c7ad155d66c4a065ce86c5c8629f26338407f7c8
VJohanWisnuaji <wisnuaji.johan@gmail.com>
2020-08-12 21:00:53 +0700

    sebelum pull

--- app/src/main/java/com/example/synrgybelanjaanemak/MainActivity.kt
+++ app/src/main/java/com/example/synrgybelanjaanemak/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.synrgybelanjaanemak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.recvi_item_belanjaan.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
